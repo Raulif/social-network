5111f97bb1ba3388f633767b1cbd5907cf29d706
raulif <raul4cade@gmail.com>
2017-11-01 10:35:03 +0100

    Initial raulif commit

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const compression = require('compression');
 
 app.use(compression());
 
+app.use(express.static(`${__dirname}/public`))
+
 if (process.env.NODE_ENV != 'production') {
     app.use('/bundle.js', require('http-proxy-middleware')({
         target: 'http://localhost:8081/'
