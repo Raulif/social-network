12ab7de23e3fe854e76aba79327845091e4c4301
raulif <raul4cade@gmail.com>
2017-11-02 07:42:09 +0100

    Add Register and Logo

--- index.js
+++ index.js
@@ -1,10 +1,57 @@
 const express = require('express');
 const app = express();
 const compression = require('compression');
+const bodyParser = require('body-parser')
+const spicedPg = require('spiced-pg');
+const cookieSession = require('cookie-session')
+const db = spicedPg(process.env.DATABASE_URL || 'postgres:rauliglesias:Fourcade1@localhost:5432/socialnetwork');
+const bcrypt = require('bcryptjs');
+const session = require('express-session')
+const csurf = require('csurf')
+
 
 app.use(compression());
 
-app.use(express.static(`${__dirname}/public`))
+app.use('/public', express.static(`${__dirname}/public`))
+app.use(cookieSession({
+    secret: 'a really hard to guess secret',
+    maxAge: 1000 * 60 * 60 * 24 * 14
+}));
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+app.use(bodyParser.json())
+// app.use(csurf())
+//
+// var hashPassword = function(plainTextPassword) {
+//     return new Promise(function(resolve, reject) {
+//         bcrypt.genSalt(function(err, salt) {
+//             if (err) {
+//                 return reject(err);
+//             }
+//             bcrypt.hash(plainTextPassword, salt, function(err, hash) {
+//                 if (err) {
+//                     return reject(err);
+//                 }
+//                 resolve(hash);
+//             });
+//         });
+//     });
+// }
+//
+// var checkPassword = function(textEnteredInLoginForm, hashedPasswordFromDatabase) {
+//     return new Promise(function(resolve, reject) {
+//         bcrypt.compare(textEnteredInLoginForm, hashedPasswordFromDatabase, function(err, doesMatch) {
+//             if (err) {
+//                 reject(err);
+//             } else {
+//                 resolve(doesMatch);
+//             }
+//         });
+//     });
+// }
+//
+console.log('in index');
 
 if (process.env.NODE_ENV != 'production') {
     app.use('/bundle.js', require('http-proxy-middleware')({
@@ -13,8 +60,83 @@ if (process.env.NODE_ENV != 'production') {
 }
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
+    if(!req.session.user) {
+        res.send('/welcome')
+    } else {
+        res.sendFile(__dirname + '/index.html');
+
+    }
 });
+
+app.get('/welcome', function(req, res){
+    if (!!req.session.user) {
+        res.redirect('/')
+    }
+    else {
+        res.sendFile(__dirname + '/index.html');
+
+    }
+});
+
+app.post('/newuser', (req, res) => {
+    if(!req.body.firstname || !req.body.lastname || !req.body.email || !req.body.password) {
+        console.log('missing fields');
+        res.json({success: false})
+    } else {
+        var firstName = req.body.firstname;
+        var lastName = req.body.lastname;
+        var email = req.body.email;
+        var password = req.body.password;
+
+        console.log(req.body);
+
+        req.session.user = {
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            password: password
+        }
+
+        const qRegisterUser = `INSERT INTO users (firstname, lastname, email, password) VALUES ($1, $2, $3, $4) RETURNING id`
+        const params = [firstName, lastName, email, password]
+        db.query(qRegisterUser, params).then(() => {
+            res.json({success: true})
+        }).catch(err => console.log("THERE WAS AN ERROR IN /postquery newuser",err));
+    }
+})
+
+app.post('/attemptlogin', (req, res) => {
+    if(!req.body.email || !req.body.password) {
+        console.log('fields missing!');
+        res.json({success: false})
+    }
+    else {
+        var email = req.body.email;
+        var password = req.body.password;
+        const qUserLogin = `SELECT * FROM users WHERE email = $1`
+        const params = [email, password]
+        db.query(qUserLogin, params).then((results) => {
+            if(results.rowCount < 1) {
+                console.log('wrong login data');
+                res.json({success: false})
+            } else {
+                let loginData = results.rows[0]
+                req.session.user = {
+                    id: loginData.user_id,
+                    firstName: loginData.firstname,
+                    lastName: loginData.lastname,
+                    email: loginData.email
+                }
+                res.json({
+                    success: true,
+                    user: req.session.user
+                })
+            }
+        })
+    }
+
+})
+
 
 app.listen(8080, function() {
     console.log("I'm listening.")
--- src/start.js
+++ src/start.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, Link, IndexRoute, hashHistory } from 'react-router';
+import { Welcome, Register, Login } from './welcome';
+import { Logo } from './logo';
+
+const isLoggedIn = location.pathname != '/welcome'
+
+let component = <Welcome />
+
+if (isLoggedIn) {
+    component = <Logo />
+}
 
 ReactDOM.render(
-    <HelloWorld />,
+    component,
     document.querySelector('main')
 );
-
-function HelloWorld() {
-    return (
-        <div>Hello, World!</div>
-    );
-}
+//
+// const router = (
+//     <Router history={hashHistory}>
+//         <Route path="/" component={Welcome}>
+//             <Route path="/login" component={Login} />
+//             <IndexRoute component={Register} />
+//   	    </Route>
+//     </Router>
+// );
+//
+// ReactDOM.render(
+//     router,
+//     document.querySelector('main')
+// );
